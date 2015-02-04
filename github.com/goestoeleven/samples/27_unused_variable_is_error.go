a352b21fb6e77d597fa9fce02ae7b22ba6dc0f63
GoesToEleven <todd.mcleod@fresnocitycollege.edu>
2015-02-04 11:51:00 -0800

    through functions as types

---
+++ github.com/goestoeleven/samples/27_unused_variable_is_error.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var message string
+	message = "Hello World."
+
+	var name string
+	name = "Medhi"
+
+	fmt.Println(message)
+	// fmt.Println(name)
+}
