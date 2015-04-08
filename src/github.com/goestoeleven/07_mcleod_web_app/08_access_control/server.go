2660f5eb083787f9a0e0f6834f495d4ce1b41141
Todd McLeod <todd.mcleod@fresnocitycollege.edu>
2015-04-08 14:16:56 -0700

    serving static more flushed out with css

--- src/github.com/goestoeleven/07_mcleod_web_app/08_access_control/server.go
+++ src/github.com/goestoeleven/07_mcleod_web_app/08_access_control/server.go
@@ -8,22 +8,13 @@ import (
 var tmpl = template.Must(template.ParseFiles(
 	"site/includes/document_top.html",
 	"site/includes/document_bottom.html",
-	"site/anybody/index.html",
-	"site/anybody/index2.html",
-	"site/assets/stylesheets/main.css"))
+	"site/index.html"))
 
 func init() {
-	http.HandleFunc("/index2.html", augh)
 	http.HandleFunc("/", root)
 	// http.HandleFunc("/sign", sign)
 }
 
-func augh(w http.ResponseWriter, r *http.Request) {
-	err := tmpl.ExecuteTemplate(w, "index2.html", nil)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-}
 func root(w http.ResponseWriter, r *http.Request) {
 	err := tmpl.ExecuteTemplate(w, "index.html", nil)
 	if err != nil {
